ee6c4aa0f0366f7e14a3513dc83b8647b9df5178
Freidrich <fredrbak@gmail.com>
2017-02-13 21:55:57 +0100

    Action rgb/luminosity

--- RoomControl/RoomControl/sequence/Action.py
+++ RoomControl/RoomControl/sequence/Action.py
@@ -1,24 +1,47 @@
-from abc import abstractmethod
+from abc import ABCMeta
+
 
 class Action(object):
+
+    __metaclass__ = ABCMeta
 
     def __init__(self, *_v):
         self.v = _v
         pass
 
 
-    @abstractmethod
-    def r(self):
-        return self.r
+# Red
+    @property
+    def r(self): return self.r
+    @r.setter
+    def r(self, _r): self.r = _r
+# Green
+    @property
+    def g(self): return self.g
+    @g.setter
+    def g(self, _g): self.g = _g
 
-    @abstractmethod
-    def g(self):
-        return self.g
+# Blue
+    @property
+    def b(self): return self.b
+    @b.setter
+    def b(self, _b): self.b = _b
 
-    @abstractmethod
-    def b(self):
-        return self.b
 
-    @abstractmethod
-    def t(self):
-        return self.t
+    def rgb(self):
+        return self.r, self.g, self.b
+
+# This would be called instead of rgb when sending to a dimmerlamp
+    def intensity(self):
+        r_wgt = .2126
+        g_wgt = .7152
+        b_wgt = .0722
+        return self.r * r_wgt
+
+
+
+
+    @property
+    def t(self): return self.t
+
+    def time(self): return self.t() # Because Paul is stupid <3
