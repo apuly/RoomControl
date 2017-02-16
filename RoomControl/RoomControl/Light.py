75d11dc26812093a36dd8d6c7af800c5998aaa06
Freidrich <fredrbak@gmail.com>
2017-02-16 13:23:01 +0100

    Action mainly structured

--- RoomControl/RoomControl/Light.py
+++ RoomControl/RoomControl/Light.py
@@ -1,5 +1,26 @@
+from abc import ABCMeta, abstractmethod
 
-class RGB(object):
+
+"""
+This class is here to join luminosity and rgb as a common class, in this way they can be used interchangably with dimmer- and rgb-lights
+"""
+class Light(metaclass = ABCMeta):
+    @staticmethod
+    def float_to_bits(f, n):
+        if 0 > f or f < 1:
+            raise Exception("Float has to be between 0 and 1")
+        return int(round(f * (2 ** n - 1), 0))
+
+    @abstractmethod
+    def rgb(self):
+        pass
+
+    @abstractmethod
+    def luminosity(self):
+        pass
+
+
+class RGB(Light):
     # Weights for luminosity conversion
     _R_WEIGHT = .2126
     _G_WEIGHT = .7152
@@ -52,7 +73,21 @@ class RGB(object):
              + self.b * self._B_WEIGHT
 # END RGB
 
-def float_to_bits(f, n):
-    if 0 > f or f < 1:
-        raise Exception("Float has to be between 0 and 1")
-    return int(round(f * (2 ** n - 1), 0))
+class Luminosity(Light):
+
+    def __init__(self, l = .0):
+        self._l = l
+
+
+    @property
+    def l(self): return self._l
+    @l.setter
+    def l(self, l): self._l = l
+
+# From parentclass
+    def rgb(self):
+        return self.l, self.l, self.l
+
+    def luminosity(self):
+        return self.l
+
