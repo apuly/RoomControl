3e63998d6c134789377f1089748e274a85a6cb75
Freidrich <fredrbak@gmail.com>
2017-02-16 13:15:46 +0100

    Action mainly structured

--- RoomControl/RoomControl/sequence/Action.py
+++ RoomControl/RoomControl/sequence/Action.py
@@ -1,25 +1,32 @@
+
+from RGB import *
+
 class Action(object):
-
-
-
-    def __init__(self, *v):
-        self.v = v
-
+    def __init__(self, *, rgb = RGB(0, 0, 0)):
+        self._rgb = rgb
         self._t = .0
 
 
-
-
-
     @property
-    def t(self): return self._t
+    def t(self):
+        return self._t
     @t.setter
-    def t(self, t): self._t = t
+    def t(self, t):
+        self._t = t
 
     @property
-    def time(self): return self.t # Because Paul is stupid <3
+    def time(self):
+        return self.t
     @time.setter
-    def time(self, t): self._t = t
+    def time(self, t):
+        self.t = t
+
+    @property
+    def rgb(self):
+        return self._rgb
+    @rgb.setter
+    def rgb(self, rgb):
+        self._rgb = rgb
 
 # for fancy actions
     def update(self, *v):
